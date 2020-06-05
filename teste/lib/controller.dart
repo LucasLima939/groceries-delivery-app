2b66b2c1ec95e9078cbaa8549def37e1ef5e614f
Lucas Lima <lucaslima939@gmail.com>
2020-06-05 19:26:17 -0300

    colocando o mobx

--- teste/lib/controller.dart
+++ teste/lib/controller.dart
@@ -1,11 +1,21 @@
 import 'package:mobx/mobx.dart';
+//part 'controller.g.dart'; //build do part: "nomeDoArquivo.g.dart"
+//
+//class Controller = ControllerBase with _$Controller;
 
-class Controller {
+abstract class ControllerBase with Store{
+  /** O Store diz que o controller está apto para
+   * receber uma geração de código baseado nele mesmo;
+   * É necessário a definição de abstract;
+   * O mapping do controller cria a código automaticamente na Classe;
+   */
 
-  var counter = Observable(0);
+  @observable
+  int counter = 0;
 
-  _increment() {
-    counter.value = counter.value + 1;
+  @action
+  increment() {
+    counter++;
   }
 
 }
